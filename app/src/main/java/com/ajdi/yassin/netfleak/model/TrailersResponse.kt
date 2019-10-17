5eb4934089bdd3639c90fcc32bc9d52383818100
Yassin AJDI <yassin.ajdi@gmail.com>
2019-10-17 17:42:13 +0100

    Add class documantations

--- app/src/main/java/com/ajdi/yassin/netfleak/model/TrailersResponse.kt
+++ app/src/main/java/com/ajdi/yassin/netfleak/model/TrailersResponse.kt
@@ -2,6 +2,10 @@ package com.ajdi.yassin.netfleak.model
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * @author Yassin Ajdi
+ * @since 10/17/2019.
+ */
 data class TrailersResponse(
 
     @SerializedName("results")
