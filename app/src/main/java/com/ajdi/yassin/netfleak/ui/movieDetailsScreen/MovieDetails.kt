f705f0ee42142d7f719df095a4792d7fd34927d7
Mehdi LAMRINI <m.lamrini@piman2-0.fr>
2019-10-19 23:06:18 +0100

    WIP screen movie details

---
+++ app/src/main/java/com/ajdi/yassin/netfleak/ui/movieDetailsScreen/MovieDetails.kt
@@ -0,0 +1,13 @@
+package com.ajdi.yassin.netfleak.ui.movieDetailsScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ajdi.yassin.netfleak.R
+
+class MovieDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
